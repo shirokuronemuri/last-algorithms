e30df15c17e98d2275d4dfa973122201d05293a0
Ihor Pavliuk <akira.pavl@gmail.com>
2026-02-14 22:48:45 +0200

    +insertion sort

--- src/day1/InsertionSort.ts
+++ src/day1/InsertionSort.ts
@@ -1,1 +1,11 @@
-export default function insertion_sort(arr: number[]): void {}
+export default function insertion_sort(arr: number[]): void {
+  for (let i = 1; i < arr.length; ++i) {
+    const curr = arr[i]!;
+    let j = i - 1;
+    while (j >= 0 && arr[j]! > curr) {
+      arr[j + 1] = arr[j]!;
+      --j;
+    }
+    arr[j + 1] = curr;
+  }
+}
