1a73c718564907503cdef0ca44fa7b3b8076d82d
Ihor Pavliuk <akira.pavl@gmail.com>
2026-02-14 22:05:56 +0200

    +two crystal balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,1 +1,16 @@
-export default function two_crystal_balls(breaks: boolean[]): number {}
+export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpLength = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpLength;
+  for (i; i < breaks.length; i += jumpLength) {
+    if (breaks[i]) {
+      i -= jumpLength;
+      break;
+    }
+  }
+
+  for (i; i < breaks.length; ++i) {
+    if (breaks[i]) return i;
+  }
+
+  return -1;
+}
