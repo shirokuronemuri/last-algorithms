3ba45dddd2fd4ed16733773ebb88331026f69807
Ihor Pavliuk <akira.pavl@gmail.com>
2026-02-17 01:16:01 +0200

    +BFS

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,1 +1,18 @@
-export default function bfs(head: BinaryNode<number>, needle: number): boolean {}
+export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const nodes: (BinaryNode<number> | null)[] = [head];
+
+  while (nodes.length > 0) {
+    const curr = nodes.shift();
+
+    if (curr?.value === needle) {
+      return true;
+    }
+    if (curr?.left) {
+      nodes.push(curr.left);
+    }
+    if (curr?.right) {
+      nodes.push(curr.right);
+    }
+  }
+  return false;
+}
