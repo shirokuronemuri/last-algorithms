cf5ccc899d97c03591f0f4eedf33e4c6c90fd770
Ihor Pavliuk <akira.pavl@gmail.com>
2026-02-18 00:30:13 +0200

    +bfs on graph matrix

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -2,4 +2,40 @@ export default function bfs(
   graph: WeightedAdjacencyMatrix,
   source: number,
   needle: number,
-): number[] | null {}
+): number[] | null {
+  const seen: boolean[] = new Array(graph.length).fill(false);
+  const prev: number[] = new Array(graph.length).fill(-1);
+
+  seen[source] = true;
+  const q: number[] = [source];
+  while (q.length > 0) {
+    const curr = q.shift()!;
+    if (curr === needle) {
+      break;
+    }
+
+    const adjs = graph[curr]!;
+    for (let i = 0; i < adjs.length; ++i) {
+      if (adjs[i] === 0 || seen[i]) {
+        continue;
+      }
+      seen[i] = true;
+      prev[i] = curr;
+      q.push(i);
+    }
+    seen[curr] = true;
+  }
+
+  if (!seen[needle]) {
+    return null;
+  }
+
+  let curr = needle;
+  const path: number[] = [];
+  while (prev[curr] !== -1) {
+    path.unshift(curr);
+    curr = prev[curr]!;
+  }
+  path.unshift(source);
+  return path;
+}
