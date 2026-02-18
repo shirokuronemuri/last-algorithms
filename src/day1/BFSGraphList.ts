c04ceff8901b1f39ee85b81ade02b307022ef92d
Ihor Pavliuk <akira.pavl@gmail.com>
2026-02-18 22:11:15 +0200

    +bfs on graph list

--- src/day1/BFSGraphList.ts
+++ src/day1/BFSGraphList.ts
@@ -2,4 +2,39 @@ export default function bfs(
   graph: WeightedAdjacencyList,
   source: number,
   needle: number,
-): number[] | null {}
+): number[] | null {
+  const seen: boolean[] = new Array(graph.length).fill(false);
+  const prev: number[] = new Array(graph.length).fill(-1);
+  seen[source] = true;
+  const q: number[] = [source];
+  while (q.length > 0) {
+    const curr = q.shift()!;
+    if (curr === needle) {
+      break;
+    }
+
+    const adjs = graph[curr]!;
+    for (const edge of adjs) {
+      if (seen[edge.to]) {
+        continue;
+      }
+      seen[edge.to] = true;
+      prev[edge.to] = curr;
+      q.push(edge.to);
+    }
+  }
+
+  if (!seen[needle]) {
+    return null;
+  }
+
+  let curr = needle;
+  const path: number[] = [];
+  while (prev[curr] !== -1) {
+    path.unshift(curr);
+    curr = prev[curr]!;
+  }
+
+  path.unshift(source);
+  return path;
+}
